78c66f084a5b6734b758e3408ba44eb79a6b24cc
dat <nhidsar@gmail.com>
2025-07-05 16:33:29 +0800

    chore: clean up of phase directories

---
+++ assets/js/access-gate.js
@@ -0,0 +1,15 @@
+/* 
+  © 2025 datsforge 
+  Proprietary – See LICENSE for details. 
+*/
+
+(function () {
+    const query = new URLSearchParams(window.location.search);
+    const allowedToken = 'forgeonly';
+    const userToken = query.get('access');
+    if (userToken !== allowedToken) {
+        window.location.href = '/';
+    } else {
+        document.body.style.visibility = 'visible';
+    }
+})();
